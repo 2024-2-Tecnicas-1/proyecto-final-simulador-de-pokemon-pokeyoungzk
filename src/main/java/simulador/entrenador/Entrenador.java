498c68ffb3843361c51df8ca9cd44337d6dd9648
AlejoPzK <alejopalis0@gmail.com>
2024-11-14 10:18:14 -0500

    Se añade el array para crear los equipos del entrenamiento de lo pokemones

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,5 +1,31 @@
 package simulador.entrenador;
 
+import java.util.ArrayList;
+import simulador.pokemon.Pokemon;
+
 public class Entrenador {
-    // TODO: Aquí va tu código
+ private String nombre;
+    private ArrayList<Pokemon> equipo;
+
+    public Entrenador(String nombre, String pokemons){
+        this.nombre=nombre;
+        this.equipo=new ArrayList<>();
+    }
+     public String getNombre(){
+        return nombre;
+    }
+    public void agregarPokemon(Pokemon pokemon){
+        equipo.add(pokemon);
+        System.out.println(pokemon.getName()+" ha sido agregado al equipo "+ this.nombre);
 }
+    public void entrenarPokemon(){
+        
+    }
+    public void mostraPokemons(){
+        
+    }
+    public void prepararBatallas(){
+        
+    }
+}
+
