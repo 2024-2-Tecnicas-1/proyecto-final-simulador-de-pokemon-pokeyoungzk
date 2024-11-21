1d98c04f948540962c357a1198085dc54cf09c03
Diego Palacios <alejopalis0@gmail.com>
2024-11-20 23:41:21 -0500

    Se añade la clase tipo pokemon con sus caracteristicas

--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -1,5 +1,19 @@
 package simulador.pokemon;
 
-public class TipoPokemon {
-    // TODO: Aquí va tu código
+import java.util.HashMap;
+import java.util.Map;
+
+public enum TipoPokemon {
+    FUEGO, AGUA, PLANTA, ELECTRICO, NORMAL, HIELO;
+
+    private static final Map<TipoPokemon, TipoPokemon> ventajas = new HashMap<>();
+
+    static {
+        ventajas.put(FUEGO, PLANTA);       
+        ventajas.put(AGUA, FUEGO);        
+        ventajas.put(PLANTA, AGUA);     
+        ventajas.put(ELECTRICO, AGUA);   
+        ventajas.put(HIELO, PLANTA);    
+    }
 }
+ 
