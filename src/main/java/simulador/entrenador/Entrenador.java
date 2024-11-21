874b652106a08d0b30679101191149d6f68a50d2
Diego Palacios <alejopalis0@gmail.com>
2024-11-20 23:39:59 -0500

    Se añade la clase entrenador con su arreglo

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,5 +1,38 @@
 package simulador.entrenador;
 
+import java.util.ArrayList;
+import simulador.pokemon.Pokemon;
+
+
 public class Entrenador {
-    // TODO: Aquí va tu código
+    private String nombre;
+    private ArrayList<Pokemon> equipo;
+
+    public Entrenador(String nombre) {
+        this.nombre = nombre;
+        this.equipo = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void agregarPokemon(Pokemon pokemon) {
+        equipo.add(pokemon);
+        System.out.println(pokemon.getNombre() + " ha sido agregado al equipo de " + nombre);
+    }
+
+    public void entrenarPokemon(Pokemon pokemon) {
+        pokemon.mejorarEstadisticas(15);
+        System.out.println(pokemon.getNombre() + " ha mejorado sus estadísticas un 15%.");
+    }
+
+    public ArrayList<Pokemon> getEquipo() {
+        return equipo;
+    }
+
+    public String Nombre() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
+
